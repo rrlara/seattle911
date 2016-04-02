4551e5b8ef2fc9ada0599f7e63b4a24e1aa8c7d0
Rene Rodriguez <rrlara@gmail.com>
2016-04-01 21:19:32 -0700

    Ok

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 var path = require('path')
+var webpack = require('webpack')
 
 module.exports = {
     entry: './src/index.js',
@@ -20,4 +21,21 @@ module.exports = {
             }
         ]
     }
+}
+if (process.env.NODE_ENV === 'production') {
+  module.exports.plugins = [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    }),
+    new webpack.optimize.OccurenceOrderPlugin()
+  ]
+} else {
+  module.exports.devtool = '#source-map'
 }
